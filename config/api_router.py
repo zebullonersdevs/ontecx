7a3e3dec7c8c413c705a3e806062e2d99a743a31
emmanuel <emmanuelukwuegbu2016@gmail.com>
2020-03-11 18:02:41 -0700

    url path for notification

--- config/api_router.py
+++ config/api_router.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
-    path("articles/", include("publications.urls"))
+    path("articles/", include("publications.urls")),
+    path("notification/", include("notification.urls"))
 ]
 
 
