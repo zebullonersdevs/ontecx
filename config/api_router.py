b4e65f25131dc0b96eea2baa968f6d8971d63b76
emmanuel <emmanuelukwuegbu2016@gmail.com>
2020-03-12 04:46:05 -0700

    new url path added

--- config/api_router.py
+++ config/api_router.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("articles/", include("publications.urls")),
-    path("notification/", include("notification.urls"))
+    path("notification/", include("notification.urls")),
+    path("user/", include("users.urls"))
 ]
 
 
