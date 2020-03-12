89d5e0c1987dfbfe594c361dc238a7b436ee9075
emmanuel <emmanuelukwuegbu2016@gmail.com>
2020-03-12 04:46:39 -0700

    updates

--- ontecx/users/urls.py
+++ ontecx/users/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 
 from ontecx.users import views as user_views
 
-app_name = "users"
-urlpatterns = [
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
+app_name = "users"
+
+urlpatterns = [
+    path('api-token-auth/', TokenObtainPairView.as_view(), name="token_auth"),
+    path('api-token-refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api-token-verify/', TokenVerifyView.as_view(), name="token_verify"),
 ]
