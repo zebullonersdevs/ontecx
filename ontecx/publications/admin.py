c986be241e4ebae055ed69664b9fae9b18a4dbdf
emmanuel <emmanuelukwuegbu2016@gmail.com>
2020-03-13 11:03:30 -0700

    Added publication category to admin panel

--- ontecx/publications/admin.py
+++ ontecx/publications/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Publication
+from .models import Publication, PublicationCategory
 
 admin.site.register(Publication)
+admin.site.register(PublicationCategory)
