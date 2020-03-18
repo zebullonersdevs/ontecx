89d1daedbb6955acecffe2a7831fbe350d19f4a5
emmanuel <emmanuelukwuegbu2016@gmail.com>
2020-03-18 03:07:31 -0700

    added replied comment model to admin panel

--- ontecx/comments/admin.py
+++ ontecx/comments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Comments
+from .models import Comments, RepliedComment
 
 admin.site.register(Comments)
+admin.site.register(RepliedComment)
